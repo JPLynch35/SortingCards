3ed897c9e2206c50ae9769ab8b4d08e70d217a70
JP Lynch <JPLynch35@gmail.com>
2018-05-09 14:48:49 -0600

    Take out argument from merge_sort tests

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -25,6 +25,7 @@ class DeckTest < Minitest::Test
     assert_equal 3, @deck.count
   end
 
+  #Test required by spec
   def test_sorting_the_deck
     card_1 = Card.new("4","Hearts")
     card_2 = Card.new("3", "Clubs")
@@ -33,6 +34,7 @@ class DeckTest < Minitest::Test
     assert_equal [card_2, card_1, card_3], deck.sorting_the_deck
   end
 
+  #Test to ensure small collection didn't just happen to sort correctly
   def test_sorting_the_deck_large
     card_1 = Card.new("4","Hearts")
     card_2 = Card.new("3", "Clubs")
@@ -45,12 +47,26 @@ class DeckTest < Minitest::Test
     assert_equal [card_4, card_2, card_1, card_3, card_7, card_5, card_6], deck.sorting_the_deck
   end
 
+  #Test required by spec
   def test_merge_sort
     card_1 = Card.new("4","Hearts")
     card_2 = Card.new("3", "Clubs")
     card_3 = Card.new("5", "Diamonds")
     deck = Deck.new([card_1, card_2, card_3])
-    assert_equal [card_2, card_1, card_3], deck.merge_sort([card_1, card_2, card_3])
+    assert_equal [card_2, card_1, card_3], deck.merge_sort
+  end
+
+  #Test to ensure small collection didn't just happen to sort correctly
+  def test_merge_sort_large
+    card_1 = Card.new("4","Hearts")
+    card_2 = Card.new("3", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    card_4 = Card.new("1","Hearts")
+    card_5 = Card.new("Jack", "Clubs")
+    card_6 = Card.new("Ace", "Diamonds")
+    card_7 = Card.new("7", "Diamonds")
+    deck = Deck.new([card_1, card_2, card_3, card_4, card_5, card_6, card_7])
+    assert_equal [card_4, card_2, card_1, card_3, card_7, card_5, card_6], deck.merge_sort
   end
 
 end
