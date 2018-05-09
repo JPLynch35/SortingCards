53d6f2a89c64ee80e61bbfe5295d75082bd1f152
JP Lynch <JPLynch35@gmail.com>
2018-05-09 16:23:46 -0600

    Remove setup so tests are easier to read

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -1,28 +1,32 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card'
-require './lib/guess'
 require './lib/deck'
 
 class DeckTest < Minitest::Test
 
-  def setup
-    @card_1 = Card.new("3","Hearts")
-    @card_2 = Card.new("4", "Clubs")
-    @card_3 = Card.new("5", "Diamonds")
-    @deck = Deck.new([@card_1, @card_2, @card_3])
-  end
-
   def test_does_it_exist
-    assert_instance_of Deck, @deck
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    deck = Deck.new([card_1, card_2, card_3])
+    assert_instance_of Deck, deck
   end
 
   def test_cards
-    assert_equal [@card_1, @card_2, @card_3], @deck.cards
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    deck = Deck.new([card_1, card_2, card_3])
+    assert_equal [card_1, card_2, card_3], deck.cards
   end
 
   def test_count
-    assert_equal 3, @deck.count
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    deck = Deck.new([card_1, card_2, card_3])
+    assert_equal 3, deck.count
   end
 
   #Test required by spec
