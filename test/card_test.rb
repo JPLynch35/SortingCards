ec4c53891f413ac2df544c77d360bac1c43c72bd
JP Lynch <JPLynch35@gmail.com>
2018-05-09 16:27:33 -0600

    Add tests for all cases of face cards

--- test/card_test.rb
+++ test/card_test.rb
@@ -10,9 +10,21 @@ class CardTest < Minitest::Test
   end
 
   def test_card_attributes
-    card = Card.new("Ace", "Spades")
-    assert_equal "14", card.value
-    assert_equal "Spades", card.suit
+    card1 = Card.new("6", "Spades")
+    assert_equal "6", card1.value
+    assert_equal "Spades", card1.suit
+    card2 = Card.new("Jack", "Spades")
+    assert_equal "11", card2.value
+    assert_equal "Spades", card2.suit
+    card3 = Card.new("Queen", "Spades")
+    assert_equal "12", card3.value
+    assert_equal "Spades", card3.suit
+    card4 = Card.new("King", "Spades")
+    assert_equal "13", card4.value
+    assert_equal "Spades", card4.suit
+    card5 = Card.new("Ace", "Spades")
+    assert_equal "14", card5.value
+    assert_equal "Spades", card5.suit
   end
 
 end
