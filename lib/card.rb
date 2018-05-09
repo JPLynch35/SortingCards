6446865aad79a3f5efb630a05d7915337db860cb
JP Lynch <JPLynch35@gmail.com>
2018-05-09 14:24:08 -0600

    Change values to strings

--- lib/card.rb
+++ lib/card.rb
@@ -3,16 +3,16 @@ class Card
   attr_reader :value, :suit
 
   def initialize(value, suit)
-    if value.to_i <= 10
-      @value = value
-    elsif value == "Jack"
-      @value = 11
+    if value == "Jack"
+      @value = "11"
     elsif value == "Queen"
-      @value = 12
+      @value = "12"
     elsif value == "King"
-      @value = 13
+      @value = "13"
     elsif value == "Ace"
-      @value = 14
+      @value = "14"
+    else
+      @value = value
     end
     @suit = suit
   end
